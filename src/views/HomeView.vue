aafabde0e37e5e4609843b73b43b743d72eba817
desasukaraja21 <saorisofyan@gmail.com>
2023-01-26 11:40:40 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Banner from '../components/Banner.vue'
+import Sidebar from '../components/Sidebar.vue'
+</script>
+
+<template>
+  <Header/>
+  <Banner/>
+  <main class="flex">
+    <Sidebar/>
+    <div class="flex-1 p-10">
+
+
+      <p class="text-3xl font-semibold">Visi & Misi Desa Sukaraja</p>
+      <p class="text-lg font-semibold mt-4">Visi Desa Sukaraja</p>
+      <p>
+        Berdasarkan kondisi saat ini dan tantangan yang akan dihadapi dalam 5 tahun mendatang serta dengan mempertimbangkan modal dasar yang dimiliki, maka Visi pembangunan Desa Sukaraja Tahun 2021-2025 adalah:
+        “Menjadikan Desa Sukaraja Bangkit untuk menjadi salah satu Desa terbaik di Kecamatan Sukaraja dengan melibatkan Partisipasi Aktif dari  Seluruh Pemangku kepentingan Desa dan Warga Masyarakat Desa Sukaraja”
+      </p>
+      <p class="text-lg font-semibold mt-4">Misi Desa Sukaraja</p>
+      <p>
+        Selain penyusunan Visi juga telah ditetapkan misi-misi yang memuat sesuatu pernyataan yang harus dilaksanakan oleh desa, agar tercapainya visi desa tersebut. Visi berada diatas Misi. Pernyataan Visi kemudian dijabarkan ke dalam Misi agar dapat dioperasionalkan/dikerjakan. Sebagaimana penyusunan Visi, Misi pun dalam penyusunannya menggunakan pendekatan partisipatif dan pertimbangan potensi dan kebutuhan Desa Sukaraja sebagaimana proses yang dilakukan, maka Misi Desa Sukaraja yaitu sebagai berikut:
+      </p>
+        <ul>
+          <li>1. Untuk Lebih mengoptimalkan peran dan fungsi RT/RW dalam Pemerintahan Desa maka perlu didorong adanya pemekaran wilayah untuk RT/RW yang penduduk diwilayahnya sudah bisa dilakukan untuk dilaksanakan untuk pembentukan RT/RW baru sehingga RT/RW sebagai mitra pemerintahan Desa yang mempunyai peran strategis dan Garda terdepan pelayanan Pemerintah Desa tersebut dapat lebih maksimal dalam melayani masyarakat.</li>
+          <li>2. Meningkatkan mutu pelayanan pemerintahan Desa Sukaraja sebagai abdi masyarakat dengan mengoptimalisasikan aparatur/petugas Desa dalam hal pelayanan ke masyarakat.</li>
+          <li>3. Mewujudkan pembangunan mental spiritual melalui peningkatan kualitas SDM, pendidikan akhlak anak sejak usia dini dengan lebih mengaktifkan peran serta PAUD, Taman Pendidikan Alqur’an (TPA) serta Pondok Pesantren yang berada di Desa Sukaraja.</li>
+          <li>4. Meningkatkan kesejahteraan masyarakat Desa dengan lebih mengoptimalkan Badan Usaha Milik Desa (BUMDes) dan program lain untuk membuka lapangan kerja bagi warga masyarakat Desa Sukaraja, serta meningkatkan produksi rumah tangga kecil.</li>
+          <li>5. Menghidupkan kembali kegiatan-kegiatan kepemudaan dan organisasi kepemudaan dengan menumbuhkan kembali Karang Taruna baik rayon maupun sub rayon sehingga dapat menggali potensi Generasi Milineal di Desa Sukaraja sehingga dapat diarahkan dan dibina sehingga peran Pemuda atau Generasi Milineal dapat lebih dimaksimalkan.</li>
+          <li>6. Mengoptimalkan fungsi dan peran dari Pondok Pesantren, Majelis Taklim, Majelis Zikir yang ada di Desa Sukaraja dan mengadakan pengajian rutin dan Perayaan Hari Besar Islam seperti Maulid Nabi (Muludan), Isra Miraj (Rajaban) di Desa.</li>
+          <li>7. Meningkatkan pembangunan infrastruktur yang mendukung dalam perekonomian desa, seperti perbaikan jalan, jembatan serta infrastruktur strategis lainnya dengan melakukan kordinasi dengan pihak kecamatan dan  dinas terkait di tingkat kabupaten.</li>
+          <li>8. Memastikan bantuan pendidikan, kesehatan  maupun sosial baik dari Pemerintah Pusat, Propinsi dan Kabupaten sampai ketangan masyarakat yang membutuhkan dan tepat sasaran.</li>
+        </ul>
+        <p class="text-lg font-semibold mt-4">Nilai-Nilai</p>
+        <p>Nilai nilai aturan yang dijunjung tinggi oleh pemerintah Desa Sukaraja dalam hal ini bertujuan bagaimana mewujudkan visi dan misi yang sudah ada untuk kesejahteraan masyarakat Desa Sukaraja, Nilai nilai itu bertujuan bagaimana memberi batasan bagi kita semua dalam menjalankan roda pemerintahan Desa Sukaraja. Nilai nilai yang diutamakan dan sudah dijalankan itu berdasarkan dan berazaskan atas azas Kebersamaan,Transparansi/Keterbukaan, Jujur, Adil, Demoktratis dan dapat dipertanggung-jawabkan. Nilai nilai tersebut harus melandasi jalannya roda pemerintahan yang dijalankan agar selalu mendapat kepercayaan dari masyarakat.</p>
+        <ol>
+          <li>1.	Transparan dibangun atas dasar kepercayaan dan kebebasan atau infomasi, adanya sifat keterbukaan bagi semua pihak yang berkepentingan terhadap semua informasi yang ada, Transaparansi dalam hal ini diatur dengan nilai nilai yang ada sehingga tidak kebablasan.</li>
+          <li>2.	Dapat dipertanggungjawabkan atau akuntable, semua apa yang dijalankan oleh pemerintah desa harus mampu dipertanggungjawabkan kepada masyarakat Desa Sukaraja khususnya dan pemerintah baik Kecamatan ataupun Kabupaten.</li>
+          <li>3.	Demokratis dalam arti memberikan kesempatan kepada semua pihak untuk berpendapat menyampaikan aspirasinya secara baik dan proporsional, tanpa ada unsur kepentingan dari berbagai golongan.</li>
+          <li>4.	Partisipatif, sistem demokrasi yang sudah berjalan memberikan kepada masyarakat untuk meyampaikan apa yang menjadi kebutuhannya, selama ini sistem yang berjalan adalah dari atas ke bawah (Top Down) sekarang menjadi bawah ke atas (Bottom Up), masyarakat harus selalu dilibatkan yang pada prinsipnya dari rakyat untuk rakyat.</li>
+          <li>5.	Profesional, dalam rangka menjalanlan roda pemerintahan, harus mampu membedakan antara Kepentingan dan kebutuhan, dalam hal memberikan pelayanan terbaik bagi masyarakat.</li>
+          <li>6.	Keadilan, apaun yang dihasilkan harus mempunyai nilai keadilan bagi masyarakat pada umumnya, dalam menjalankan roda pemerintahan yang ada beban pekerjaan harus disesuaikan dengan tupoksi masing masing perangkat yang ada yang sudah disesuikan dengan SOTK.</li>
+          <li>7.	Kesetaraan dan Keadilan Gender, Seluruh elemen yang ada baik dalam masyarakat ataupun pemerintah harus mampu bersikap tidak membedakan perlakuan dan kesempatan atas dasar jenis Kelamin dan oreintasi sseksual.</li>
+          <li>8.	Kelestarian Lingkungan, Penyelenggaran dan pelaksanaan pembangunan harus di selaraskan dengan kondisi lingkungan yang ada dan harus mampu menciptakan situasi dan kehidupan sosial yang ramah lingkungan.</li>
+        </ol>
+      <p class="text-3xl font-semibold my-4">Struktur Desa Sukaraja</p>
+      <img src="@/assets/struktur-desa-sukaraja.png" alt=""/>
+    </div>
+  </main>
+</template>
+
+<style>
+  body{
+    background: #f1f1f1;
+  }
+</style>
